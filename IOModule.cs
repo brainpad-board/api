bcc34ae1bd6877fc4908328e9f647542a01b5d04
Dat Tran <dat.tran@ghielectronics.com>
2021-06-19 11:40:46 -0400

    No overload except for Out

--- IOModule.cs
+++ IOModule.cs
@@ -6,22 +6,10 @@ using System.Threading;
 namespace GHIElectronics.TinyCLR.Drivers.BrainPadController {
     public abstract class IOModule : IDisposable {
         public virtual double In() => 0;
-        public virtual void Out(double data) {
-
-        }
-
-        public virtual void Out(double[] data) {
-
-        }
-        public virtual void Out(byte[] data) {
-
-        }
-
+        public virtual void Out(double data) { }
+        public virtual void Out(double[] data) { }
         public virtual double OutIn(byte[] data, byte[] result) => 0;
-
-        public virtual double In(byte[] result) => 0.0;
         public abstract void Dispose(bool disposing);
-
         public void Dispose() {
             this.Dispose(true);
             GC.SuppressFinalize(this);
