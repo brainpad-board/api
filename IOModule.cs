792df8fc8922be62b591cf71c073143af777713b
Dat Tran <dat.tran@ghielectronics.com>
2021-08-19 12:38:01 -0400

    Change OutIn from byte array to double.

--- IOModule.cs
+++ IOModule.cs
@@ -5,7 +5,7 @@ namespace BrainPad {
         public virtual double In() => 0;
         public virtual void Out(double data) { }
         public virtual void Out(double[] data) { }
-        public virtual double OutIn(byte[] data, byte[] result) => 0;
+        public virtual double OutIn(double[] data, double[] result) => 0;
         public abstract void Dispose();
     }
 }
